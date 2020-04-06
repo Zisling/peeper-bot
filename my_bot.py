3cf913d0b29b9a2a2cca29162f8dc196cdf2dc57
Zisling <43644953+Zisling@users.noreply.github.com>
2020-04-06 17:53:14 +0300

    Initial commit

---
+++ my_bot.py
@@ -0,0 +1,193 @@
+# url = "https://api.telegram.org/bot1242526231:AAF_E6iJo8Gw0GpTjgOI3k2wGlJJZQzWeYI/"
+import json
+import telebot
+import my_bot_res
+from telebot import types
+
+bot = telebot.TeleBot("1242526231:AAF_E6iJo8Gw0GpTjgOI3k2wGlJJZQzWeYI")
+
+
+def check_add(message):
+    print(str(message.text).split(' ').__len__() >= 3)
+    if str(message.text).split(' ').__len__() >= 3:
+        return True
+    else:
+        bot.send_message(message.chat.id,
+                         "incorrect format you need to enter /AddTask [your subject] [your Task description]")
+        return False
+
+
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    print(message)
+    bot.reply_to(message, message.from_user.first_name + """ you need some hot 🌶 so call to your local farmer""")
+
+
+@bot.message_handler(commands=['corona'])
+def corona(message):
+    print(message)
+    bot.send_animation(message.chat.id, open('corona.gif', 'rb'))
+
+
+@bot.message_handler(commands=['random_vid', 'Random_vid'])
+def vid(message):
+    try:
+        print(message)
+        to_send = my_bot_res.random_vid()
+        reply_to_step(message, to_send)
+    except Exception as e:
+        print(e)
+        bot.reply_to(message, 'oooops server time out')
+
+
+@bot.message_handler(commands=['random_gif', 'Random_gif'])
+def gif(message):
+    try:
+        print(message)
+        bot.send_animation(message.chat.id, open(my_bot_res.random_gif(), 'rb'))
+    except Exception as e:
+        print(e)
+        bot.reply_to(message, 'oooops server time out')
+
+
+@bot.message_handler(commands=['Help', 'help'])
+def send_list_of_commands(message):
+    bot.reply_to(message, """
+    /start\n/Help\n/Videos\n/duck\n/corona\n/Random_gif\n/Random_vid
+    """)
+
+
+@bot.message_handler(func=lambda m: str(m.text).lower() == 'almog almog')
+def almog_send(message):
+    bot.reply_to(message, 'Almog! Almog!')
+
+
+@bot.message_handler(commands=['Videos'])
+def send_hw(message):
+    videos = my_bot_res.list_of("my_videos")
+    str = ""
+    for x in videos:
+        str = str + '\n' + x
+    bot.reply_to(message, str)
+
+# /AddTask [subject] [Task description]
+# @bot.message_handler(func=check_add, commands=['AddTask'])
+# def send_task(message):
+#     data = str(message.text).split(' ', 2)
+#     my_bot_res.add_hw(data[1], data[2])
+#     bot.reply_to(message, "i have added to %s the task %s" % (data[1], data[2]))
+
+
+@bot.message_handler(commands=['ivy'])
+def ivy(message):
+    bot.send_photo(message.chat.id, open('ivy.png', 'rb'))
+    bot.send_message(message.chat.id, "or zivivy")
+    bot.send_photo(message.chat.id, open('zivivy.png', 'rb'))
+    msg = bot.send_message(message.chat.id, "give as the answer now")
+    bot.register_next_step_handler(msg, the_ivy_question)
+
+
+def the_ivy_question(message):
+    try:
+        answer = message.text
+        if answer == 'zivivy':
+            bot.send_message(message.chat.id, 'good boy')
+        else:
+            bot.send_message(message.chat.id, 'bad boy go and think on what you done')
+            bot.send_photo(message.chat.id, open('angry_dog.png', 'rb'))
+    except Exception as e:
+        bot.reply_to(message, 'oooops')
+
+
+@bot.message_handler(commands=['duck'])
+def duck_debug(message):
+    print(message.from_user.first_name)
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    itembtn1 = types.KeyboardButton('yellow')
+    itembtn2 = types.KeyboardButton('red')
+    itembtn3 = types.KeyboardButton('blue')
+    itembtn4 = types.KeyboardButton('white')
+    itembtn5 = types.KeyboardButton('the big duck up in the sky')
+    markup.add(itembtn1, itembtn2, itembtn3, itembtn4, itembtn5)
+    bot.send_photo(message.chat.id, open('4ducks.jpg', 'rb'))
+    msg = bot.send_message(message.chat.id, "Choose a duck to start debugging:", reply_markup=markup)
+    bot.register_next_step_handler(msg, duck2)
+
+
+def duck2(message):
+    try:
+        markup = types.ReplyKeyboardRemove(selective=False)
+        bot.send_message(message.chat.id, "you chosen %s" % message.text, reply_markup=markup)
+        answer = message.text
+        itembtn1 = types.KeyboardButton('Step')
+        markup2 = types.ReplyKeyboardMarkup(row_width=1)
+        markup2.add(itembtn1)
+        if answer == 'yellow' or answer == "red" or answer == 'white' or answer == 'the big duck up in the sky' or answer == 'blue':
+            duck = my_bot_res.get_self(message.chat.id, answer)
+            reply = duck.step()
+            msg = multiple_reply(message, reply, markup2)
+            bot.register_next_step_handler(msg, duck_step)
+        else:
+            bot.send_message(message.chat.id, 'not writen yet')
+    except Exception as e:
+        my_bot_res.unregister(message.chat.id)
+        print(e)
+        bot.reply_to(message, 'oooops')
+
+
+def duck_step(message):
+    try:
+        markup = types.ReplyKeyboardRemove(selective=False)
+        answer = message.text
+        if answer == 'Step':
+            duck = my_bot_res.get_self(message.chat.id)
+            itembtn1 = types.KeyboardButton('Step')
+            markup2 = types.ReplyKeyboardMarkup(row_width=1)
+            markup2.add(itembtn1)
+            reply = duck.step()
+            if reply != 'DONE':
+                msg = multiple_reply(message, reply, markup2)
+                bot.register_next_step_handler(msg, duck_step)
+            else:
+                duck.unregister()
+                bot.send_message(message.chat.id, "The end", reply_markup=markup)
+                bot.send_animation(message.chat.id, open('community_dance.gif', 'rb'))
+                print('end')
+        else:
+            my_bot_res.unregister(message.chat.id)
+            bot.send_message(message.chat.id, "The end only step can be done in debug mod", reply_markup=markup)
+            print("end badly")
+    except Exception as e:
+        print(e)
+        my_bot_res.unregister(message.chat.id)
+        bot.reply_to(message, 'oooops')
+
+
+def reply_to_step(msg, reply, markup=None):
+    if reply[0:5] == 'text:':
+        return bot.send_message(msg.chat.id, reply[5:], reply_markup=markup)
+    elif reply[0:4] == 'img:':
+        return bot.send_photo(msg.chat.id, open(reply[4:], 'rb'), reply_markup=markup)
+    elif reply[0:4] == 'gif:':
+        return bot.send_animation(msg.chat.id, open(reply[4:], 'rb'), reply_markup=markup)
+    elif reply[0:4] == 'vid:':
+        return bot.send_video(msg.chat.id, open(reply[4:], 'rb'), reply_markup=markup)
+    elif reply[0:6] == 'audio:':
+        return bot.send_audio(msg.chat.id, open(reply[6:], 'rb'), reply_markup=markup)
+    elif reply[0:4] == 'sti:':
+        return bot.send_sticker(msg.chat.id, open(reply[4:], 'rb'), reply_markup=markup)
+    else:  # return as text if dont find any type
+        return bot.send_message(msg.chat.id, reply, reply_markup=markup)
+
+
+# split on '\n'
+# replay to all messages in the reply form duck step
+def multiple_reply(msg, m_reply, markup):
+    ret = msg
+    lines = str(m_reply).split('\n')
+    for x in lines:
+        ret = reply_to_step(msg, x, markup)
+    return ret
+
+
+bot.polling()
---
+++ my_bot_res.py
@@ -0,0 +1,105 @@
+import json
+import re
+import sqlite3
+import glob
+import random
+
+list = {"my_videos": (
+    'https://www.youtube.com/watch?v=nzeLzlbjszQ', 'https://www.youtube.com/watch?v=1Pf53iFqm4Y',
+    'https://www.youtube.com/watch?v=_3aeQKOQd7I')}
+
+story = {
+    "yellow": ('text:the yellow duck is smiling at you like a creep 🙂',
+               'text:you need to run for your life',
+               'text:run the duck is after you',),
+    "red": ('text:you go to your shower with your trusted red duck',
+            'text:you start singing in the shower and then...',
+            'text:a sword cut the duck down',
+            'text:you start a fight with 12 different duck ninja',
+            'text:and you lose and die while ivy is watching you\nimg:ivy.png',
+            ),
+    "blue": ('img:blue1.jpg',
+             'img:blue2.jpg',
+             'img:blue3.jpg',
+             'img:blue4.jpg',
+             ),
+    "white": ('text:dont blink\nimg:angel1.jpg',
+              'text:why did you blink\nsti:angel2.webp',
+              'text:run for your life',
+              ),
+    "the big duck up in the sky": ('img:the_big_duck_in_the_sky.png',)
+
+}
+
+
+def random_vid():
+    list_vid = glob.glob('random_vid/*.mp4')
+    list_vid.extend(list['my_videos'])
+    ret = random.choice(list_vid)
+    x = re.match('.+\.mp4', str(ret))
+    if x is not None and x.span()[1] == str(ret).__len__():
+        return 'vid:' + str(ret)
+    else:
+        return 'text:' + str(ret)
+
+
+def random_gif():
+    list_gif = glob.glob('*.gif')
+    return random.choice(list_gif)
+
+
+story_chat_id_map = {
+
+}
+
+
+def get_self(chat_id, st=""):
+    if story_chat_id_map.keys().__contains__(chat_id):
+        return story_chat_id_map[chat_id]
+    else:
+        new_duck = my_duck_step(chat_id, story[st])
+        new_duck.register()
+        return new_duck
+
+
+def unregister(chat_id):
+    if story_chat_id_map.keys().__contains__(chat_id):
+        story_chat_id_map.pop(chat_id)
+    else:
+        print('no chat_id is in')
+
+
+def list_of(list_name):
+    return list[list_name]
+
+
+def add_hw(subject, task):
+    if not list.__contains__(subject):
+        list[subject] = (str(task),)
+    else:
+        list[subject] = list[subject] + (str(task),)
+
+
+class my_duck_step:
+    def __init__(self, chat_id, message=("bug",), step_number=0):
+        self.chat_id = chat_id
+        self.step_number = step_number
+        self.message = message
+
+    def step(self):
+        if self.step_number < self.message.__len__():
+            msg = self.message[self.step_number]
+            self.step_number += 1
+            return msg
+        else:
+            return "DONE"
+
+    def register(self):
+        if not story_chat_id_map.keys().__contains__(self.chat_id):
+            story_chat_id_map[self.chat_id] = self
+        else:
+            print("problem at register")
+
+    def unregister(self):
+        if story_chat_id_map.keys().__contains__(self.chat_id):
+            story_chat_id_map.pop(self.chat_id)
